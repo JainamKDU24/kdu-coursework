2d6f2bbf674574f2e441eca0ee3738f32d5cb3be
JainamKDU24 <jainamrajiv.gandhi@kickdrumtech.com>
2024-02-26 14:20:06 +0530

    Frontend Homework-14

---
+++ frontend/homework/14/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Form } from './Impl/Form'
+import {Scroll} from './Impl/Scroll'
+import {Timer} from './Impl/Timer'
+
+function App() {
+  return (
+    <>
+      <Form />
+      <Timer />
+      <Scroll />
+    </>
+  )
+}
+
+export default App
